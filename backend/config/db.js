84a975adca1bbf5b2a0c84814cef6400cae4865a
Swagan-2005 <swathigangatharan2005@gmail.com>
2026-07-14 20:47:54 +0530

    change

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,7 +4,7 @@ const mongodb=async()=>{
     try{
         
         await mongoose.connect(process.env.MONGO_URL)
-        console.log("DB Connected")
+        console.log("DB Connected Successfully")
     }
     catch(err){
         console.log("DB is not connected")
