10a3feb0366ced4691b8bc3cbf1a88a27cb4be2b
Swagan-2005 <swathigangatharan2005@gmail.com>
2026-07-14 20:01:05 +0530

    url change

--- Frontend/src/Home.jsx
+++ Frontend/src/Home.jsx
@@ -17,15 +17,15 @@ function Home() {
       new bootstrap.Tooltip(el);
     });
     const datacount=async()=>{
-          let datas=await axios.get("http://localhost:3000/api/trainerget");
+          let datas=await axios.get(`${import.meta.env.VITE_API_URL}/api/trainerget`);
           settcount(datas.data.length)
     }
     const coursecount=async()=>{
-      let datas=await axios.get("http://localhost:3000/api/getcourse");
+      let datas=await axios.get(`${import.meta.env.VITE_API_URL}/api/getcourse`);
       setccount(datas.data.length)
     }
     const studentcount=async()=>{
-      let datas=await axios.get("http://localhost:3000/api/getstudent");
+      let datas=await axios.get(`${import.meta.env.VITE_API_URL}/api/getstudent`);
       setscount(datas.data.length)
     }
     datacount();
